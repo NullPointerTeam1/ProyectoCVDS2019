a98cbfaa1cc0988c37eb03c7391732a35be545e6
Jose Luis Gomez Camacho <josemania10@gmail.com>
2019-11-14 20:52:51 -0500

    Reservar un recurso -Agregar elementos necesarios a la base de datos inicio

--- Base de datos/PoblarOK.sql
+++ Base de datos/PoblarOK.sql
@@ -7,3 +7,10 @@ insert into user_roles(correo, role_name) values('prueba.2019@mail.escuelaing.ed
 insert into usuarios(carnet, nombre, apellido, carrera, correo, rol, contrasena)values('2148781', 'jose', 'gomez', 'sistemas', 'jose.gomez-ca@mail.escuelaing.edu.co', 'A', '123456789');
 insert into usuarios(carnet, nombre, apellido, carrera, correo, rol, contrasena)values('2148782', 'jordi', 'nino', 'sistemas', 'jordi.el-nino@mail.escuelaing.edu.co', 'u', '123456789');
 insert into usuarios(carnet, nombre, apellido, carrera, correo, rol, contrasena)values('2148783', 'prueba', '2019', 'sistemas', 'prueba.2019@mail.escuelaing.edu.co', 'u', '123456789');  
+
+insert into tipo_recurso(id,descripcion)values(1 ,'1. Equipo de Computo');
+insert into tipo_recurso(id,descripcion)values(2 ,'2. Sala de Estudio');
+insert into tipo_recurso(id,descripcion)values(3 ,'3. Equipo Multimedia');
+insert into tipo_recurso(id,descripcion)values(4 ,'Equipo de Computo');
+insert into tipo_recurso(id,descripcion)values(5 ,'Sala de Estudio');
+insert into tipo_recurso(id,descripcion)values(6 ,'Equipo Multimedia');
--- Base de datos/Xpoblar.sql
+++ Base de datos/Xpoblar.sql
@@ -2,3 +2,6 @@ delete from usuarios;
 delete from user_roles;
 delete from recursos;
 delete from tipo_recurso;
+
+select * from recursos;
+select * from tipo_recurso;
